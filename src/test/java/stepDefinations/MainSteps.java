8996f819f55f75e550c631cb6832229be1566a03
suraj <suraj777patil@gmail.com>
2025-09-19 06:29:21 +0530

    first bdd commit

---
+++ src/test/java/stepDefinations/MainSteps.java
@@ -0,0 +1,33 @@
+package stepDefinations;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MainSteps {
+    @Given("User is on NetBanking Landing Page")
+    public void user_is_on_net_banking_landing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User login into application")
+    public void user_login_into_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Home page is displayed")
+    public void home_page_is_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("Cards are displayed")
+    public void cards_are_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
