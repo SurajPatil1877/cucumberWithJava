cced1eae8c3f6d58aa27de35346be64ffafe4499
suraj <suraj777patil@gmail.com>
2025-09-25 05:40:30 +0530

    Reusable exampled added

--- src/test/java/features/FirstProgram.feature
+++ src/test/java/features/FirstProgram.feature
@@ -5,3 +5,10 @@ Feature: Application Login
     When User login into application
     Then Home page is displayed
     And Cards are displayed
+
+    #Reusable
+  Scenario: User Page default Login
+    Given User is on NetBanking Landing Page
+    When User login into application with "john_doe" and "password123"
+    Then Home page is displayed
+    And Cards are displayed
--- src/test/java/stepDefinations/MainSteps.java
+++ src/test/java/stepDefinations/MainSteps.java
@@ -8,26 +8,27 @@ import io.cucumber.java.en.When;
 public class MainSteps {
     @Given("User is on NetBanking Landing Page")
     public void user_is_on_net_banking_landing_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User is on NetBanking Landing Page");
     }
 
     @When("User login into application")
     public void user_login_into_application() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User login into application");
     }
 
     @Then("Home page is displayed")
     public void home_page_is_displayed() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Home page is displayed");
     }
 
     @And("Cards are displayed")
     public void cards_are_displayed() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Cards are displayed");
+    }
+
+    @When("User login into application with {string} and {string}")
+    public void user_login_into_application_with_and(String username, String password) {
+        System.out.println("User login into application with " + username + " and " + password);
     }
 
 }
