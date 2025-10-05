65498174091c2b30e33cce26cd7c07d8c30271ab
suraj <suraj777patil@gmail.com>
2025-10-06 05:22:04 +0530

    data driven scenario added

--- src/test/java/features/FirstProgram.feature
+++ src/test/java/features/FirstProgram.feature
@@ -12,3 +12,23 @@ Feature: Application Login
     When User login into application with "john_doe" and password "password123"
     Then Home page is displayed
     And Cards are displayed
+
+  Scenario Outline: User Page default Login
+    Given User is on NetBanking Landing Page
+    When User login into application with "<username>" and password "<password>"
+    Then Home page is displayed
+    And Cards are displayed
+    Examples:
+      | username | password |
+      | user1    | pass123  |
+      | user2    | pass456  |
+      | user3    | pass789  |
+
+  Scenario: User Page default Signup
+    Given User is on Practice Landing Page
+    When User signup into application with following details
+      | firstName | Suraj          |
+      | lastName  | Patil          |
+      | email     | test@gmail.com |
+      | mobile    | 9878676543     |
+    Then Home page is displayed
--- src/test/java/stepDefinations/MainSteps.java
+++ src/test/java/stepDefinations/MainSteps.java
@@ -1,9 +1,12 @@
 package stepDefinations;
 
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
+import java.util.Map;
 
 public class MainSteps {
     @Given("User is on NetBanking Landing Page")
@@ -11,10 +14,15 @@ public class MainSteps {
         System.out.println("User is on NetBanking Landing Page");
     }
 
-    @When("User login into application with {string} and password {string}")
+    @When("^User login into application with (.+) and password (.+)$")
     public void user_login_into_application_with_and(String username, String password) {
         System.out.println("User login into application with " + username + " and " + password);
     }
+
+//    @When("User login into application with {string} and password {string}")
+//    public void user_login_into_application_with_and(String username, String password) {
+//        System.out.println("User login into application with " + username + " and " + password);
+//    }
 
     @Then("Home page is displayed")
     public void home_page_is_displayed() {
@@ -24,5 +32,18 @@ public class MainSteps {
     @And("Cards are displayed")
     public void cards_are_displayed() {
         System.out.println("Cards are displayed");
+    }
+
+    @Given("User is on Practice Landing Page")
+    public void userIsOnPracticeLandingPage() {
+        System.out.println("Landing Page");
+    }
+
+    @When("User signup into application with following details")
+    public void userSignupIntoApplicationWithFollowingDetails(Map<String, String> data) {
+        System.out.println(data.get("firstName"));
+        System.out.println(data.get("lastName"));
+        System.out.println(data.get("email"));
+        System.out.println(data.get("mobile"));
     }
 }
